36a7b267289e2fb617f3f4cb38f4488ed32b6992
Anna Parshukova <annpaarshukova041@gmail.com>
2024-10-09 19:08:50 +0300

    summary

---
+++ my-ts-project/src/basic/3.ts
@@ -0,0 +1,9 @@
+let someValue: string | number;
+someValue = 18;
+someValue = "AN";
+
+type EnableOrDisable = "enable" | "disable";
+let toggle: EnableOrDisable;
+
+toggle = "disable";
+toggle = "enable";
